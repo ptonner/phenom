abfdffa9034c5f6f4f29b86f6f7b9a634078039b
Peter Tonner <peter.tonner@gmail.com>
2017-12-12 14:53:27 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,82 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='phenom',
+
+    version='0.0.1a',
+
+    description='A hierarchical non-parametric microbial phenotype model',
+    long_description=long_description,
+
+    url='https://github.com/ptonner/phenom',
+
+    author='Peter Tonner',
+    author_email='peter.tonner@duke.edu',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+
+		'Environment :: Console',
+
+        'Intended Audience :: Developers',
+		'Intended Audience :: Science/Research',
+        'Topic :: Database :: Database Engines/Servers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='statistics microbiology',
+
+    packages=find_packages(exclude=['data', 'tests']),
+
+    install_requires=['pandas', 'matplotlib', 'numpy', 'pystan', 'patsy'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'popmachine=popmachine.__main__:main',
+    #     ],
+    # },
+)
