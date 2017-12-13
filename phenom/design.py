ba803cfa316838b1ebfeb4ebdabb8446322671b2
Peter <peter.tonner@duke.edu>
2017-12-13 14:33:56 -0500

    formatting, fix formula names

--- phenom/design.py
+++ phenom/design.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
 import patsy
+import re
+from copy import copy
 
 from itertools import product
+
 
 class Design(object):
 
@@ -38,7 +41,7 @@ class Design(object):
 
     @property
     def frame(self):
-        return pd.DataFrame(self.matrix, columns = self.names)
+        return pd.DataFrame(self.matrix, columns=self.names)
 
     @property
     def priors(self):
@@ -52,6 +55,7 @@ class Design(object):
 
     def __add__(self, other):
         return Add(self, other)
+
 
 class Add(Design):
 
@@ -78,7 +82,7 @@ class Add(Design):
         p1 = self.d1.priors
         p2 = self.d2.priors
 
-        return p1.tolist() + (p2+max(p1)+1).tolist()
+        return p1.tolist() + (p2 + max(p1) + 1).tolist()
 
 
 class Kron(Design):
@@ -94,7 +98,7 @@ class Kron(Design):
         m1, m2 = self.d1.matrix, self.d2.matrix
 
         for i in range(self.n):
-            ret.append(np.kron(m1[i, :], m2[i, :]))
+            ret.append(np.kron(m1[i,:], m2[i,:]))
 
         return np.array(ret)
 
@@ -106,13 +110,14 @@ class Kron(Design):
         priors = []
 
         for v1, v2 in product(p1, p2):
-            priors.append(v1 + v2*(max(p1)+1))
+            priors.append(v1 + v2 * (max(p1) + 1))
 
         return priors
 
     def _names(self):
 
-        return ['(%s: %s)x(%s: %s)'%(self.d1.name, v1, self.d2.name, v2) for v1, v2 in product(self.d1.names, self.d2.names)]
+        return ['(%s: %s)x(%s: %s)' % (self.d1.name, v1, self.d2.name, v2) for v1, v2 in product(self.d1.names, self.d2.names)]
+
 
 class Formula(Design):
 
@@ -125,7 +130,19 @@ class Formula(Design):
         return np.array(self.d)
 
     def _names(self):
-        return self.d.design_info.column_names
+        # return self.d.design_info.column_names
+        cols = copy(self.d.design_info.column_names)
+
+        # convert all categorical factors to something pretty
+        pat = 'C\((?P<factor>[a-zA-Z0-9]+)(?P<ignore>, Treatment\([a-zA-Z0-9.]+\))?\)\[T?\.?(?P<level>[a-zA-Z0-9. ]+)\]'
+        comp = re.compile(pat)
+        found = map(comp.findall, cols)
+
+        for i in range(len(cols)):
+            if len(found[i]) > 0:
+                cols[i] = ', '.join(['%s=%s' % (f, l) for f, _, l in found[i]])
+
+        return cols
 
     def _priors(self):
         priors = -1 * np.ones(self.k)
