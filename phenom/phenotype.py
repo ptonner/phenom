a893a301cb57a5d629b1cc5b7466010505fd4e2f
Peter <peter.tonner@duke.edu>
2017-12-16 16:39:31 -0500

    fix saving

--- phenom/phenotype.py
+++ phenom/phenotype.py
@@ -2,6 +2,7 @@ import numpy as np
 import pystan
 import os
 import pickle
+import pandas as pd
 
 
 class Phenotype(object):
@@ -86,19 +87,44 @@ class Phenotype(object):
         return x, y, xnorm, ynorm
 
     def save(self, d):
-        os.makedirs(os.path.join(d, 'samples'))
+        if not os.path.exists(os.path.join(d, 'samples')):
+            os.makedirs(os.path.join(d, 'samples'))
 
         self.data.to_csv(os.path.join(d, 'data.csv'))
         self.design.meta.to_csv(os.path.join(d, 'meta.csv'))
+        self.design.frame.to_csv(os.path.join(d, 'design.csv'))
 
-        pickle.dump(self, open(os.path.join(d, 'phenotype.pkl'), 'wb'))
+        x, y, xnorm, ynorm = self.normalize()
+        pd.DataFrame(y, index=x).to_csv(os.path.join(d, 'data-normalized.csv'))
+
+        # pickle.dump(self, open(os.path.join(d, 'phenotype.pkl'), 'wb'))
 
         for i, p in enumerate(self.posteriors):
+            samp = p.extract()
+
+            # unnormalized
+            x, y, xnorm, ynorm = self.normalize()
+            ymean, ystd = ynorm
+            xmin, xmax = xnorm
+
+            samp['f-native'] = samp['f'] * ystd
+            samp['f-native'][:, 0] += ymean
+
+            samp['df-native'] = samp['df'] * ystd / (xmax - xmin)
+
             pickle.dump(
-                p, open(os.path.join(d, 'samples', 'posterior_%d.pkl' % i), 'wb'))
+                samp, open(os.path.join(d, 'samples', 'posterior_%d.pkl' % i), 'wb'))
+
+            summary = p.summary()
+            summary = pd.DataFrame(
+                summary['summary'],
+                index=summary['summary_rownames'],
+                columns=summary['summary_colnames'])
+            summary.to_csv(os.path.join(d, 'samples', 'posterior_%d.csv' % i))
 
     def samples(self, *args, **kwargs):
         cfg = self.config()
         samp = self.model.sampling(data=cfg, *args, **kwargs)
+
         self.posteriors.append(samp)
         return samp
