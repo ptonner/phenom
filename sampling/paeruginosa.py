29e8315fd973a420f75520de84ac6b867c417c87
Peter Tonner <peter.tonner@nist.gov>
2020-07-28 09:44:56 -0400

    update paer sampling

--- sampling/paeruginosa.py
+++ sampling/paeruginosa.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("acid", choices=["benzoate", "citric", "malic"])
-    parser.add_argument("model", choices=["mnull", "mbatch", "mfull"])
+    parser.add_argument(
+        "model",  choices=["mnull", "mbatch", "mfull"]
+    )
     parser.add_argument("--adapt_delta", type=float, default=0.8)
     parser.add_argument("--max_treedepth", type=int, default=10)
     parser.add_argument("--dataset", default=None)
@@ -16,8 +18,12 @@ if __name__ == "__main__":
         "../data/pseudomonas/PA01-{}/".format(args.acid)
     )
     ds.meta["mMAcid"] = ds.meta["mM-acid"]
-    ds.data = ds.data.iloc[5::3, :]
+    ds.data = ds.data.iloc[6::3, :]
     ds.log()
+
+    if args.dataset is not None:
+        ds.data = ds.data.loc[:, ds.meta.plate.str.replace(" ", "_") == args.dataset]
+        ds.meta = ds.meta.loc[ds.meta.plate.str.replace(" ", "_") == args.dataset, :]
 
     # designs
     formula = """1 + C(mMAcid, Sum, levels=[5, 10, 20, 0]) 
@@ -33,31 +39,33 @@ if __name__ == "__main__":
 
     # phenotype
     if args.model == "mnull":
-        phen = phenom.phenotype.Phenotype(ds.data, mnull, model="phenom.stan")
+        phen = phenom.phenotype.Phenotype(ds.data, mnull, model="phenom_deriv.stan")
     elif args.model == "mbatch":
         phen = phenom.phenotype.Phenotype(
             ds.data,
             mbatch,
             model="phenom_deriv.stan",
-            lengthscale_priors=[[2.5, 0.45]] * 4 + [[100, 20]] * 4,
+            lengthscale_priors=[[2, 3]] * 4 + [[10, 1]] * 4,
             alpha_priors=[[10, 10]] * 4 + [[7, 10]] * 4,
             minExpectedCross=0.01,
             maxExpectedCross=10,
             sigma_prior=[0.02, 20],
         )
-    else:
+    elif args.model == "mfull":
         phen = phenom.phenotype.Phenotype(
             ds.data,
             mbatch,
             model="phenom_marginal.stan",
-            lengthscale_priors=[[2.5, 0.45]] * 4 + [[100, 20]] * 4,
+            lengthscale_priors=[[2, 3]] * 4 + [[10, 1]] * 4,
             alpha_priors=[[10, 10]] * 4 + [[7, 10]] * 4,
             minExpectedCross=0.01,
             maxExpectedCross=10,
             sigma_prior=[0.02, 20],
-            marginal_lengthscale_prior=[4, 1],
-            marginal_alpha_prior=[2, 20],
+            marginal_lengthscale_prior=[2, 1],
+            marginal_alpha_prior=[5, 50],
         )
+    else:
+        raise ValueError("Unknown model: {}!".format(args.model))
 
     # sampling
     samples = phen.samples(
